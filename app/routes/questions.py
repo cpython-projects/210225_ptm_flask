96b0e0a8a40be8f3e7dea6af01ca57de0f0faa5c
OST <cpython.projects@gmail.com>
2025-08-28 11:36:52 +0300

    28-08-25

--- app/routes/questions.py
+++ app/routes/questions.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from app.models.questions import Question
+from app.models import db
 
 
 questions_bp = Blueprint('questions', __name__, url_prefix='/questions')
@@ -9,7 +11,9 @@ def get_questions():
     """
     Returns a list of all questions.
     """
-    return "Список всех вопросов"
+    questions = Question.query.all()
+    data = [{'id': item.id, 'question': item.question} for item in questions]
+    return jsonify(data)
 
 
 @questions_bp.route('/', methods=['POST'])
@@ -17,7 +21,15 @@ def create_question():
     """
     Creates a new question.
     """
-    return "Вопрос создан"
+    data = request.get_json()
+    if not data or 'question' not in data:
+        return jsonify({'error': 'No text provided'}), 400
+
+    question = Question(question=data['question'])
+    db.session.add(question)
+    db.session.commit()
+
+    return jsonify({'id': question.id, 'question': question.question}), 201
 
 
 @questions_bp.route('/<int:id>', methods=['GET'])
@@ -25,7 +37,13 @@ def get_question(id):
     """
     Returns a question by id.
     """
-    return f"Детали вопроса {id}"
+    question = Question.query.get(id)
+    if not question: # question is None
+        return jsonify({'error': 'Question with that id does not exist'}), 404
+
+    return jsonify({
+        'id': question.id,
+        'question': question.question}), 200
 
 
 @questions_bp.route('/<int:id>', methods=['PUT'])
@@ -33,7 +51,24 @@ def update_question(id):
     """
     Updates a question by id.
     """
-    return f"Вопрос {id} обновлен"
+    question = Question.query.get(id)
+    if not question:
+        return jsonify({'error': 'Question with that id does not exist'}), 404
+
+    data = request.get_json()
+    if not data or 'question' not in data:
+        return jsonify({'error': 'No question text provided'}), 400
+
+    text = data['question'].strip()
+    if not text:
+        return jsonify({'error': 'No question text provided'}), 400
+
+    question.question = text
+    db.session.commit()
+    # return jsonify({'id': question.id, 'question': question.question}), 200
+    return jsonify({'message': 'Question updated successfully'}), 200
+
+
 
 
 @questions_bp.route('/<int:id>', methods=['DELETE'])
@@ -41,6 +76,10 @@ def delete_question(id):
     """
     Deletes a question by id.
     """
-    return f"Вопрос {id} удален"
+    question = Question.query.get(id)
+    if not question: # if question is None
+        return jsonify({'error': 'Question with that id does not exist'}), 404
 
-
+    db.session.delete(question)
+    db.session.commit()
+    return jsonify({'message': 'Question deleted successfully'}), 200
